733ba476c1b432e2475c1bcf5a7cd7d3861c1fed
DeeMATT <mattode@outlook.com>
2019-09-24 03:54:13 +0100

    Added movie & person manager class/model

--- django/core/models.py
+++ django/core/models.py
@@ -33,6 +33,7 @@ class Movie(models.Model):
         through='Role',
         related_name='acting_credits',
         blank=True)
+    objects = MovieManager()
 
     class Meta:
         ordering = ('-year', 'title')
@@ -49,7 +50,7 @@ class Person(models.Model):
     born = models.DateField()
     died = models.DateField(null=True,
                             blank=True)
-
+    objects = PersonManager()
 
     class Meta:
         ordering = (
@@ -80,3 +81,24 @@ class Role(models.Model):
         unique_together = ('movie',
                             'person',
                             'name')
+
+
+class PersonManager(models.Manager):
+    def all_with_prefetch_movies(self):
+        qs = self.get_queryset()
+        return qs.prefetch_related(
+            'directed',
+            'writing_credits',
+            'role_set__movie)
+
+
+class MovieManager(models.Manager):
+
+    def all_with_related_persons(self):
+        qs = self.get_queryset()
+        qs = qs.select_related(
+            'director')
+        qs = qs.prefetch_related(
+            'writers', 'actors')
+        return qs
+
